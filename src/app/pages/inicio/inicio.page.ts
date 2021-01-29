ac9c24ac52a1fcd6fb0bcf6adfc6f9cd646d1c3d
Faustino Delpoggio <f.delpoggio>
2021-01-29 18:46:27 -0300

    sección-04

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+
+  componentes: Componente[] = [
+    {
+      icon: 'car-sport-outline',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'alert-outline',
+      name: 'Alert',
+      redirectTo: '/action-sheet'
+    },
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
